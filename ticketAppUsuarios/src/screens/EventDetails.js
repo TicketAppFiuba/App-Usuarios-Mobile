8e00e74fd3842a539a29a93f71f759fad5875bfb
romanvazquez <roman.vazquez@cardinalsisa.com>
2023-05-26 21:43:28 -0300

    Agrega spinner de carga en detalle de eventos

--- ticketAppUsuarios/src/screens/EventDetails.js
+++ ticketAppUsuarios/src/screens/EventDetails.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { View, Image, Text, StyleSheet, ScrollView, TouchableOpacity } from 'react-native';
+import { View, Image, Text, StyleSheet, ScrollView, TouchableOpacity, ActivityIndicator } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import { API_BASE_URL } from '../constant.js';
 
@@ -18,63 +18,58 @@ const EventDetails = ({ route, navigation }) => {
   const [event, setEvent] = useState();
   const [reservationId, setReservationId] = useState(null);
   const [booked, setBooked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    AsyncStorageFunctions.getData('token')
-    .then((token) => {
-
-      fetch(`${API_BASE_URL}/user/event?event_id=${event_id}`, {
-        headers: { authorization: `Bearer ${token}` }
-      })
-      .then((response) => response.json())
-      .then((data) => {
-          mappedEvent = {
-            id: data.Event.id,
-            title: data.Event.title,
-            date: GetDayOfWeek(data.Event.date),
-            image: data.Images[0]?.link ?? 'https://i.imgur.com/UYiroysl.jpg',
-            category: data.Event.category,
-            address: data.Event.direction,
-            description: JSON.parse(data.Event.description)["blocks"][0]["text"],
-            agenda: data.Diary.map((section) => {
+    AsyncStorageFunctions.getData('token').then((token) => {
+      Promise.all([
+        fetch(`${API_BASE_URL}/user/event?event_id=${event_id}`, {
+          headers: { authorization: `Bearer ${token}` },
+        }).then((response) => response.json()),
+        fetch(`${API_BASE_URL}/user/reservations`, {
+          headers: { authorization: `Bearer ${token}` },
+        }).then((response) => response.json()),
+      ])
+        .then(([eventData, reservationsData]) => {
+          const mappedEvent = {
+            id: eventData.Event.id,
+            title: eventData.Event.title,
+            date: GetDayOfWeek(eventData.Event.date),
+            image: eventData.Images[0]?.link ?? 'https://i.imgur.com/UYiroysl.jpg',
+            category: eventData.Event.category,
+            address: eventData.Event.direction,
+            description: JSON.parse(eventData.Event.description)['blocks'][0]['text'],
+            agenda: eventData.Diary.map((section) => {
               return {
                 time: section.time,
-                activity: section.description
-              }
+                activity: section.description,
+              };
             }),
-            faqs: data.FAQ.map((faq) => {
+            faqs: eventData.FAQ.map((faq) => {
               return {
                 question: faq.question,
-                answer: faq.response
-              }
-            })
-          }
+                answer: faq.response,
+              };
+            }),
+          };
           setEvent(mappedEvent);
-      })
-      .catch((error) => {
-          console.error(error);
-      })
-    
 
-
-      fetch(`${API_BASE_URL}/user/reservations`, {
-        headers: { authorization: `Bearer ${token}` }
-      })
-      .then((response) => response.json())
-      .then((data) => {
-        data.forEach((event) => {
-          if (event.Event.id === event_id) {
-              setReservationId(event.Reservation.code)
-              setBooked(true)
-          }
+          reservationsData.forEach((event) => {
+            if (event.Event.id === event_id) {
+              setReservationId(event.Reservation.code);
+              setBooked(true);
+            }
+          });
         })
-      })
-      .catch((error) => {
-        console.error("Reservations from user error: ", error);
-      })
-    })
+        .catch((error) => {
+          console.error(error);
+        })
+        .finally(() => {
+          setIsLoading(false);
+        });
+    });
   }, []);
-    
+
   const handleReservation = () => {
     if (booked) {
       navigation.navigate('Ticket', {
@@ -83,36 +78,35 @@ const EventDetails = ({ route, navigation }) => {
         title: event?.title,
         date: event?.date,
         address: event?.address,
-        })
-      return
+      });
+      return;
     }
-    AsyncStorageFunctions.getData('token')
-    .then((token) => {
+    AsyncStorageFunctions.getData('token').then((token) => {
       fetch(`${API_BASE_URL}/user/event/reservation`, {
         method: 'POST',
-        body: JSON.stringify({  event_id: event?.id, tickets: 1 }),
-        headers: { 
+        body: JSON.stringify({ event_id: event?.id, tickets: 1 }),
+        headers: {
           authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        }
+          'Content-Type': 'application/json',
+        },
       })
-      .then((response) => response.json())
-      .then((data) => {
-        setReservationId(data.code)
-        setBooked(true)
-        navigation.navigate('Ticket', {
-          event_id: event?.id,
-          booked: booked,
-          title: event?.title,
-          date: event?.date,
-          address: event?.address, 
-          })
-      })
-      .catch((error) => {
-        console.error("Making reservation error:", error);
-      })
-    })
-  }
+        .then((response) => response.json())
+        .then((data) => {
+          setReservationId(data.code);
+          setBooked(true);
+          navigation.navigate('Ticket', {
+            event_id: event?.id,
+            booked: booked,
+            title: event?.title,
+            date: event?.date,
+            address: event?.address,
+          });
+        })
+        .catch((error) => {
+          console.error('Making reservation error:', error);
+        });
+    });
+  };
 
   const handleOpenDenunciaModal = () => {
     setDenunciaModalVisible(true);
@@ -125,11 +119,23 @@ const EventDetails = ({ route, navigation }) => {
   const toggleOptions = () => {
     setShowOptions(!showOptions);
   };
+
+  if (isLoading) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#007AFF" />
+      </View>
+    );
+  }
+
   return (
     <ScrollView style={styles.container}>
-      <Image source={{ uri: event?.image ? event?.image : "https://i.imgur.com/UYiroysl.jpg" }} style={styles.image} />
+      <Image
+        source={{ uri: event?.image ? event?.image : 'https://i.imgur.com/UYiroysl.jpg' }}
+        style={styles.image}
+      />
       <TouchableOpacity style={styles.optionsButton} onPress={toggleOptions}>
-        <Ionicons name="ellipsis-vertical-outline" size={32} color="white" style={styles.optionsIcon}/>
+        <Ionicons name="ellipsis-vertical-outline" size={32} color="white" style={styles.optionsIcon} />
       </TouchableOpacity>
       <TouchableOpacity style={styles.shareButton}>
         <ShareButton />
@@ -164,12 +170,11 @@ const EventDetails = ({ route, navigation }) => {
         ))}
       </View>
       <TouchableOpacity style={styles.buttonContainer} onPress={() => handleReservation()}>
-        { !booked? (
-            <Text style={styles.buttonText}>Reservar Entrada</Text>
-          ) : (
-            <Text style={styles.buttonText}>Ver Ticket</Text>
-          )
-        }
+        {!booked ? (
+          <Text style={styles.buttonText}>Reservar Entrada</Text>
+        ) : (
+          <Text style={styles.buttonText}>Ver Ticket</Text>
+        )}
       </TouchableOpacity>
 
       <DenunciaModal visible={isDenunciaModalVisible} onClose={handleCloseDenunciaModal} />
@@ -178,6 +183,11 @@ const EventDetails = ({ route, navigation }) => {
 };
 
 const styles = StyleSheet.create({
+  loadingContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   container: {
     flex: 1,
     backgroundColor: 'white',
@@ -282,7 +292,7 @@ const styles = StyleSheet.create({
     right: 30,
     borderRadius: 8,
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
-  }
+  },
 });
 
 export default EventDetails;
