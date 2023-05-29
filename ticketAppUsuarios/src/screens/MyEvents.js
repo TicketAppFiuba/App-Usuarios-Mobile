eb095547c96e88bfd5221618492b2152d9e28dc9
Tomas Arrachea <tomas.arrachea@zerf.tech>
2023-05-29 01:48:45 -0300

    handle eventos sin fotos

--- ticketAppUsuarios/src/screens/MyEvents.js
+++ ticketAppUsuarios/src/screens/MyEvents.js
@@ -90,7 +90,7 @@ const MyEvents = ({ navigation }) => {
             id: event.id,
             title: event.title,
             date: GetDayOfWeek(event?.Event.date),
-            image: event?.Images[0].link  ?? 'https://i.imgur.com/UYiroysl.jpg',
+            image: event?.Images[0]?.link  ?? 'https://i.imgur.com/UYiroysl.jpg',
             distance: Math.ceil(event.Distance),
             category: event.category,
           }));
